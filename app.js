4d7e829ddf49142331ff7ad86bdbfab246670b7d
nishagupta912274 <nishagupta912274@gmail.com>
2026-05-28 20:46:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+const color = ['white', 'red', 'green', 'orange', 'aqua', 'crimson', 'blue'];
 
+let i = 0;
+
+let box = document.getElementById('box');
+let btn = document.getElementById('btn');
+let colorName = document.getElementById('colorName');
+
+function change() {
+
+    if (i < color.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+
+    btn.innerHTML = 'Change ' + i;
+
+    box.style.backgroundColor = color[i];
+
+    colorName.innerText = color[i];
+}
